e632ae24927c32aad5224cf7a10d75cac08c758e
zohabi <abdollahzz1381@gmail.com>
2023-07-21 17:32:50 +0330

    write setHeight and setWidth

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -3,14 +3,21 @@ public class Rectangle {
     private int width;
 
     public Rectangle(int height, int width) {
+        if(height>=0 & width>=0){
         this.height = height;
-        this.width = width;
+        this.width = width;}
     }
     public int getHeight(){
       return this.height;
     }
     public int getWidth(){
         return this.width;
+    }
+    public void setWidth(int width){
+        this.width=width;
+    }
+    public void setHeight(int height){
+        this.height=height;
     }
     public int AreaRec() {
         return getHeight() * getWidth();
