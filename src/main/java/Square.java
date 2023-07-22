f7b15a39936ccdc78ef4ad4edfcf525f211c6040
Alireza Ziaee <alrz1999@gmail.com>
2023-07-22 05:53:57 +0330

    Add Validation for Square length

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -2,7 +2,15 @@ public class Square {
     private int length;
 
     public Square(int length) {
+        validateLength(length);
+
         this.length = length;
+    }
+
+    private static void validateLength(int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getLength() {
