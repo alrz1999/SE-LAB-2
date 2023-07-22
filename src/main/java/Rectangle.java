5d620bc4dc72902ac6fca350475c7599e6215da6
Alireza Ziaee <alrz1999@gmail.com>
2023-07-22 06:18:29 +0330

    Add validation for Rectangle

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -3,9 +3,26 @@ public class Rectangle implements Shape {
     private int width;
 
     public Rectangle(int height, int width) {
-        if (height >= 0 & width >= 0) {
-            this.height = height;
-            this.width = width;
+        validate(height, width);
+
+        this.height = height;
+        this.width = width;
+    }
+
+    private static void validate(int height, int width) {
+        validateHeight(height);
+        validateWidth(width);
+    }
+
+    private static void validateWidth(int width) {
+        if (width < 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateHeight(int height) {
+        if (height < 0) {
+            throw new IllegalArgumentException();
         }
     }
 
