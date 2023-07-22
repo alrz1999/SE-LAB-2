ed22f0a4ab93c2051d923c92bd7372e25cdbacf6
Alireza Ziaee <alrz1999@gmail.com>
2023-07-22 06:05:42 +0330

    Reformat Rectangle.java code

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -3,21 +3,26 @@ public class Rectangle implements Shape {
     private int width;
 
     public Rectangle(int height, int width) {
-        if(height>=0 & width>=0){
-        this.height = height;
-        this.width = width;}
+        if (height >= 0 & width >= 0) {
+            this.height = height;
+            this.width = width;
+        }
     }
-    public int getHeight(){
-      return this.height;
+
+    public int getHeight() {
+        return this.height;
     }
-    public int getWidth(){
+
+    public int getWidth() {
         return this.width;
     }
-    public void setWidth(int width){
-        this.width=width;
+
+    public void setWidth(int width) {
+        this.width = width;
     }
-    public void setHeight(int height){
-        this.height=height;
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     @Override
